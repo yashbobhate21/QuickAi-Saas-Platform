e2c51e2e1363e881e2016a94f5f2c3f62e2a71cf
yash bobhate <yashbobhate2194@gmail.com>
2025-07-16 15:42:30 +0530

    built some components

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
+import Aitools from '../Components/Aitools'
 
 function Home() {
     return (
        <>
        <Navbar/>
        <Hero/>
+       <Aitools/>
        </>
     )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -14,6 +14,6 @@ createRoot(document.getElementById('root')).render(
    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
   </ClerkProvider>
 )
